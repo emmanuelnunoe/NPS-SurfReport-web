44d88c544836974745130a39045aeebf3d65a256
emmanuelnunoe <emmanuel.nuno.estrella@uabc.edu.mx>
2025-01-23 08:00:11 -0800

    added card to surf-reportpage

--- nps-surfreport-web/pages/surfreport.vue
+++ nps-surfreport-web/pages/surfreport.vue
@@ -1,5 +1,18 @@
+<script setup lang="ts">
+    import { customers } from '~/testingdata/customer.json'
+</script>
+
 <template>
     <div>
         <h1>NPS | Surf Report</h1>
+        <Card v-for=" customer in customers" :key="customer.id" :="customer"/>
     </div>
 </template>
+
+<style scoped>
+.grid{
+    display: grid;
+    grid-template-columns: repeat(auto-fit, minmax(18rem, 1fr));
+    gap: 2rem;
+}
+</style>
