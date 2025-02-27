620611043d09c822b55c74c60210d2e94d0f4932
Emmanuel Nuño <emmanuel.nuno.estrella@uabc.edu.mx>
2025-02-26 22:20:47 -0800

    add bootsrap and video file

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,9 +1,16 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+
 export default defineNuxtConfig({
   target: 'static',
   router: {
-    base: '/NPS-SurfReport-web/'
+    base: 'https://emmanuelnunoe.github.io/NPS-SurfReport-web/NPS-SurfReport-web/'
   },
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
+    css: [
+      'bootstrap/dist/css/bootstrap.min.css'
+    ],
+    plugins: [
+      { src: '~/plugins/bootstrap.js', mode: 'client' }
+    
 })
