3e3e2284021a5269824f6dde6a20006d1c5f6641
Emmanuel Nuño <emmanuel.nuno.estrella@uabc.edu.mx>
2025-02-26 17:32:37 -0800

    new config file

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -11,5 +11,27 @@ export default defineNuxtConfig({
   ],
   plugins: [
     '~/plugins/bootstrap.js',
-  ]
+  ],
+  head: {
+    link: [
+      {
+        rel: 'stylesheet',
+        href: 'https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css'
+      }
+    ],
+    script: [
+      {
+        src: 'https://code.jquery.com/jquery-3.5.1.slim.min.js',
+        type: 'text/javascript'
+      },
+      {
+        src: 'https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js',
+        type: 'text/javascript'
+      },
+      {
+        src: 'https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js',
+        type: 'text/javascript'
+      }
+    ]
+  }
 })
