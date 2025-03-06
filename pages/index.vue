f227d46c2d8104dcdee2da58196186b3ace44a6c
Emmanuel Nuño <emmanuel.nuno.estrella@uabc.edu.mx>
2025-03-05 20:57:31 -0800

    update index title

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,7 @@
 <template>
     <div>
-        <h1>Welcome to NPS Surf Forecast App</h1>
+        <h1>See conditions, for sea sessions.</h1>
+        <h2>NPS | Surf Report</h2>
         <VIdeoBackground />
     </div>
 </template>
@@ -16,6 +17,26 @@ export default {
 </script>
 
 <style scoped>
+h1{
+    margin-top: 20%;
+    text-align: center;
+    font-size: 3em;
+    font-weight: 800;
+    color: white;
+}
+
+h1, h2{
+  font-family: 'Lucida Sans', 'Lucida Sans Regular', 'Lucida Grande', 'Lucida Sans Unicode', Geneva, Verdana, sans-serif;
+}
+
+h2{
+    margin-top: 5%;
+    text-align: center;
+    font-size: 1.5em;
+    font-weight: 400;
+    color: rgb(44, 44, 44);
+}
+
 .content {
   position: relative;
   z-index: 1; /* To ensure the content stays above the video */
